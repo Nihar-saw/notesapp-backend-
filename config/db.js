cb0fc40f2f8cfac8a7c46848222f44fba554745c
Nihar-sw <nihar11sawant@gmail.com>
2026-05-07 11:39:50 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export default connectDB;
